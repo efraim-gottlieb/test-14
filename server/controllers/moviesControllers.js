5bc31a5b4f0ca14f24a56f99b4888ac91d0df74d
EfraimGottlieb <efgo583208979@gmail.com>
2026-03-05 11:25:23 +0200

    added seats property to movie object

--- server/controllers/moviesControllers.js
+++ server/controllers/moviesControllers.js
@@ -1,7 +1,10 @@
-import fs from 'fs/promises'
-
+import fs from "fs/promises";
 
 export async function getMovies(req, res) {
-  const movies = await fs.readFile("./data/movies.json", "utf-8");
-  res.send(JSON.parse(movies));
+  const movies = JSON.parse(await fs.readFile("./data/movies.json", "utf-8"));
+  const seats = JSON.parse(await fs.readFile("./data/seats.json", "utf-8"));
+  movies.forEach((movie) => {
+    movie.seats = seats[movie.imdbID];
+  });
+  res.send(movies);
 }
